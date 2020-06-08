bf049def004850a7cf8bdcf0d45301155143498f
trk-wakil <new_trk@hotmail.com>
2020-06-08 12:51:06 -0400

    CheckPoint
    works but still need to resolve the CORS issue. ALSO, a lot of test code added here. remove it

--- Client/cleanerCode.js
+++ Client/cleanerCode.js
@@ -161,7 +161,6 @@ class ApiHelper {
         this.baseURL = 'https://localhost:44355/api/Game/';
         this.cardsFromServer;
         this.maxNumOfCardsAllowed = 0;
-
     }
 
     async getMaxPlayableCardsFromServer() {
@@ -178,9 +177,9 @@ class ApiHelper {
     }
 
         
-    async goFetchCards(numOfUniqueCards, playerName) {
+    async goFetchCards(numOfUniqueCards) {
         //TODO use the operation in fetch and allow using the variable
-        var uri = this.baseURL + 'StartNewGame/0';
+        var uri = this.baseURL + 'GetCardsForNewGame/0';
         var result = await (await fetch(uri)).json();
         return result;
     }
@@ -338,8 +337,122 @@ async function ready() {
 
     document.getElementById('PlayAsGuestBtn').addEventListener('click', () => { gameManager.beginNewGameAsNoOne(); });
 
-    document.getElementById('NewGameBtn').addEventListener('click', () => { gameManager.beginNewGame(); });
+    document.getElementById('PlayAsMeBtn').addEventListener('click', () => { gameManager.beginNewGame(); });
     document.getElementById('ResumeGameBtn').addEventListener('click', () => { gameManager.resumeGame(); });
     document.getElementById('EndGameBtn').addEventListener('click', () => { gameManager.endGame(); });
 
 }
+
+
+
+function MiscApiTest() {
+    var uri = 'https://localhost:44355/api/Game/TestInputVal/';
+    //var uri = 'https://localhost:44355/api/Game/GetCardCount/';
+    //var uri = 'https://localhost:44355/api/Game/GetCardsForNewGame/';
+    var requestData = {numOfCards: 3};
+        
+    var req = {
+        method: 'POST',
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        headers: {
+          'Content-Type': 'application/json'   //,
+          //'Content-Type': 'application/x-www-form-urlencoded'
+        }  //,
+        //body: JSON.stringify(requestData)
+    };
+
+    fetch(uri, req)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log('from Server:  ' +data);
+            });
+
+    // fetch(uri, req)
+    //     .then((response) => {
+    //         return response.json()
+    //     })
+    //     .then((output) => {
+    //         console.log('from Server:  ' +output);
+    //     });
+            
+
+}
+
+
+
+function tryAjax() {
+    var uri = 'https://localhost:44355/api/Game/TestInputVal/';
+    var requestData = {numOfCards: 3};
+
+
+    $.ajax({
+            type: "POST",
+            //crossdomain: true,
+            headers: { 'Access-Control-Allow-Origin': '*' },
+            url: uri,
+            //dataType: "json",
+            data: JSON.stringify(requestData),
+            success: function (result) {
+                console.log(result)
+            },
+            error: function (xhr, status, err) {
+                console.error(xhr, status, err);
+            }
+        });
+}
+
+
+/*********
+ * 
+ * 
+ 
+
+
+ $.ajax({
+            type: "POST",
+            crossdomain: true,
+            url: "http://localhost:1415/anything",
+            dataType: "json",
+            data: JSON.stringify({
+                anydata1: "any1",
+                anydata2: "any2",
+            }),
+            success: function (result) {
+                console.log(result)
+            },
+            error: function (xhr, status, err) {
+                console.error(xhr, status, err);
+            }
+        });
+
+
+
+
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+  
+  postData('https://example.com/answer', { answer: 42 })
+    .then(data => {
+      console.log(data); // JSON data parsed by `response.json()` call
+    });
+
+
+    ******************************/
